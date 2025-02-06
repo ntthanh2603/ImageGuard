0dd01fb022e1c0f1dabbec33f5c7b94caca4ec69
Liam-2603 <tuanthanh2kk4@gmail.com>
2025-02-06 11:33:26 +0700

    refactor

--- crawl_data/sex_photos.py
+++ crawl_data/sex_photos.py
@@ -18,43 +18,13 @@ def get_images(driver, url, index):
     close_noti = driver.find_element(By.XPATH, f'//*[@class="tbclose-btn"]')
     close_noti.click()
 
-    # scroll = -1000
-    # scroll_times = 10
-    # for _ in range(scroll_times):
-    #     pyautogui.scroll(scroll)
-    #     time.sleep(1)
-    # scroll_amount = 3000  # Số pixel cuộn mỗi lần
-    # scroll_times = 10   # Số lần cuộn
-
-    # for _ in range(scroll_times):
-    #     # Sử dụng JavaScript để cuộn trang
-    #     driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
-    #     time.sleep(1)
-
-
     images = driver.find_elements(By.CLASS_NAME, 'aligncenter')
-    # images = driver.find_elements(By.XPATH, '//*[@class="aligncenter"]')
 
     time.sleep(5)
 
     number = len(images)
     print(f'==> URL: {url}')
     print(f'Number of photos on url: {number}')
-
-
-    # for i, img in enumerate(images):
-    #         try:
-    #             img_url = img.get_attribute('src')
-    #             time.sleep(1)
-    #             if img_url:
-    #                 response = requests.get(img_url)
-    #                 time.sleep(1)
-    #                 if response.status_code == 200:
-    #                     with open(f'../data/sex_photos/image_{i}.jpg', 'wb') as f:
-    #                         f.write(response.content)
-    #                         time.sleep(1)
-    #         except Exception as e:
-    #             print(f"Lỗi khi tải ảnh {i}: {str(e)}")
     
     for img in images:
         try:
